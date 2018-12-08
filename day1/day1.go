a76cc6f5feb9fcd8ac17387667bbef6c37663a97
Felix Fritz <felixfritz@Felixs-MBP-2.fritz.box>
2018-12-08 21:53:49 +0100

    day 1 solved

---
+++ day1/day1.go
@@ -0,0 +1,10 @@
+package day1
+
+import "github.com/jassler/aoc18/fileparser"
+
+// Start explores this problem set: https://adventofcode.com/2018/day/1
+func Start(inputPath string, ch chan<- string) {
+	numbers := fileparser.ToIntArray(inputPath)
+	go part1(numbers, ch)
+	go part2(numbers, ch)
+}
---
+++ day1/day1part1.go
@@ -0,0 +1,14 @@
+package day1
+
+import "fmt"
+
+// basic premis is: calculate sum of each row
+func part1(numbers []int, ch chan<- string) {
+	result := 0
+
+	for _, num := range numbers {
+		result += num
+	}
+
+	ch <- fmt.Sprint("Part 1: ", result)
+}
